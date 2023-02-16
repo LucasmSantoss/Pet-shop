a6e42953d50554dd6d74e4439026793d323c2ffd
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-02-16 15:17:33 -0300

    estructura pre-finality

---
+++ assets/scripts/farmacia.js
@@ -0,0 +1,30 @@
+import { filtradoDeFarmacia, agregarProductos, busquedaDeProductos, mostrarError } from "../module/functions.js";
+
+const productos = document.getElementById("productos")
+
+const buscador = document.getElementById("search")
+
+fetch("https://mindhub-xj03.onrender.com/api/petshop")
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data);
+        const dataCategoria = data.filter(producto => producto.categoria === "farmacia");
+        console.log(dataCategoria);
+        agregarProductos(filtradoDeFarmacia(data, dataCategoria), productos)
+
+
+        buscador.addEventListener("keyup", () => {
+            let buscadorValue = buscador[0].value.toLowerCase();
+            console.log(buscadorValue)
+            let filtroDeBusqueda = busquedaDeProductos(filtradoDeFarmacia(data, dataCategoria), buscadorValue);
+            filtradoDeFarmacia(filtroDeBusqueda, productos);
+            mostrarError(filtroDeBusqueda, productos);
+        });
+
+
+        buscador.addEventListener("submit", (e) => {
+            e.preventDefault();
+        });
+    }).catch((err) => {
+        console.log(err)
+    })
---
+++ assets/scripts/jugeteria.js
@@ -0,0 +1,29 @@
+import { filtradoDeJugueteria, agregarProductos, busquedaDeProductos, mostrarError } from "../module/functions.js";
+
+const productos = document.getElementById("productos")
+
+const buscador = document.getElementById("search")
+
+fetch("https://mindhub-xj03.onrender.com/api/petshop")
+    .then(response => response.json())
+    .then((data) => {
+
+        const dataCategoria = data.filter(producto => producto.categoria === "jugueteria");
+
+        agregarProductos(filtradoDeJugueteria(data, dataCategoria), productos)
+
+        buscador.addEventListener("keyup", () => {
+            let buscadorValue = buscador[0].value.toLowerCase();
+            console.log(buscadorValue)
+            let filtroDeBusqueda = busquedaDeProductos(filtradoDeJugueteria(data, dataCategoria), buscadorValue);
+            filtradoDeJugueteria(filtroDeBusqueda, productos);
+            mostrarError(filtroDeBusqueda, productos);
+        });
+
+        buscador.addEventListener("submit", (e) => {
+            e.preventDefault();
+        });
+
+    }).catch((err) => {
+        console.log(err)
+    })
